50d8c84b44e1e14a1267766bdc272f2b4d8fc9e4
ansh kumar <mirtunjay111976@gmail.com>
2026-08-28 17:47:41 +0530

    sum of 2 num

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+    int num1, num2;
+
+    //input two number seprated by space 
+    scanf("%d %d", &num1, &num2);
+
+    //display the sum matching the simple output format
+    printf ("sum= %d\n", num1 + num2);
+
+    return 0;
+}
